607a44bd1c2edfe41b730d41b9ce28cb97c9caed
Arthur Cai <arthurca3607@gmail.com>
2024-01-22 21:28:16 -0600

    cube rotation

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( {color: 0x006400} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
